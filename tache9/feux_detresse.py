0821eb8e1271655cfc2cb87b64e566911537cd55
AnthonyEperonnier <anthony.eperonnier@gmail.com>
2026-06-10 16:16:57 +0200

    Tache9 : feux de détresse pour la suite

---
+++ tache9/feux_detresse.py
@@ -0,0 +1,54 @@
+import time
+import sys
+sys.path.append('/home/vigiris/adeept_picar-b2/task1_leds')
+sys.path.append('/home/vigiris/adeept_picar-b2/examples')
+
+from control_leds import setup, set_led, all_off
+from importlib import import_module
+ws2812 = import_module('06_Spi_WS2812')
+Adeept_SPI_LedPixel = ws2812.Adeept_SPI_LedPixel
+
+NB_LEDS = 14
+led = Adeept_SPI_LedPixel(count=NB_LEDS, bright=255)
+led.setDaemon(True)
+led.start()
+
+def feux_detresse_on():
+    set_led(6, 1)   # left_R
+    set_led(7, 1)   # right_R
+    for i in range(NB_LEDS):
+        led.set_led_color(i, 255, 0, 0)  # rouge pur
+    time.sleep(0.5)
+    set_led(6, 0)
+    set_led(7, 0)
+    for i in range(NB_LEDS):
+        led.set_led_color(i, 0, 0, 0)
+    time.sleep(0.5)
+
+def feux_off():
+    all_off()
+    for i in range(NB_LEDS):
+        led.set_led_color(i, 0, 0, 0)
+
+if __name__ == '__main__':
+    try:
+        setup()
+        print("Feux de détresse ON — Ctrl+C pour arrêter")
+        while True:
+            feux_detresse_on()
+    except KeyboardInterrupt:
+        feux_off()
+        print("Feux éteints.")
+def feux_detresse_on():
+    set_led(4, 1)   # left_R
+    # set_led(5, 1) # left_G  <- supprimé
+    set_led(7, 1)   # right_R
+    # set_led(8, 1) # right_G <- supprimé
+    for i in range(NB_LEDS):
+        led.set_led_color(i, 255, 0, 0)  # rouge pur
+    time.sleep(0.5)
+    set_led(4, 0)
+    set_led(7, 0)
+    for i in range(NB_LEDS):
+        led.set_led_color(i, 0, 0, 0)
+    time.sleep(0.5)
---
+++ tache9/feux_obstacle.py
@@ -0,0 +1,4 @@
+import drive
+
+if __name__ == "__main__":
+       drive. manual_test()
